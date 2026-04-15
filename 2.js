68304d01e7ac3cef071e09b259de5b8279be7361
huzaifa-wq <bhaihuzaifa733@gmail.com>
2026-04-15 17:14:25 +0500

    ok

---
+++ 2.js
@@ -0,0 +1,19 @@
+
+let target = document.getElementById("target");
+
+
+let li1 = document.createElement("li");
+li1.textContent = "First item";
+target.appendChild(li1);
+
+
+let li2 = document.createElement("li");
+li2.textContent = "Second item";
+
+li2.classList.add("my-item");
+target.appendChild(li2);
+
+
+let li3 = document.createElement("li");
+li3.textContent = "Third item";
+target.appendChild(li3);
